6ba0a199d438cc321984c281c6a16d797d576b8d
fuu3629 <fuku7229@outlook.jp>
2025-04-08 16:18:14 +0900

    publicがないといけなかったのでfavicon設置

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link href='/favicons/favicon.png' rel='icon' />
+      </Head>
       <body style={{ backgroundColor: '#e3e3fd' }}>
         <Main />
         <NextScript />
