2d803b3b44b8ebb24be3f58b499d5a9b9997cdb5
fuu3629 <fuku7229@outlook.jp>
2025-04-10 19:00:24 +0900

    ちょっと変更

--- apps/assets/scripts/image_gen.py
+++ apps/assets/scripts/image_gen.py
@@ -7,13 +7,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 client = OpenAI()
-client.api_key = os.getenv("OPENAI_API_KEY")  # API keyのセット
+client.api_key = os.getenv("OPENAI_API_KEY")
 f = open('prompt.txt', 'r')
 prompt = f.read()
 f.close()
 response = client.images.generate(
-  model="dall-e-3",  # モデル
-  prompt=prompt,  # プロンプト
+  model="dall-e-3", 
+  prompt=prompt, 
   n=1,  # 生成数
   size="1792x1024",  # 解像度 dall-e-3では1024x1024、1792x1024、1024x1792
   response_format="b64_json",  # レスポンスフォーマット url or b64_json
@@ -21,8 +21,6 @@ response = client.images.generate(
   style="vivid"  # スタイル vivid or natural
 )
 
-# 画像保存
-# ファイル名にはタイムスタンプと通番を含めています
 for i, d in enumerate(response.data):
     with open(f"images/dall-e-3_{int(time.time())}_{i}.png", "wb") as f:
         f.write(base64.b64decode(d.b64_json))
