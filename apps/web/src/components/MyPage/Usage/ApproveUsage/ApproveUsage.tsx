ff85c7a22c057b9e11cd42c191e9bf2496b9b34b
Yuma769 <miyuma1209@gmail.com>
2025-05-14 22:14:43 +0900

    スタイル崩れ修正

--- apps/web/src/components/MyPage/Usage/ApproveUsage/ApproveUsage.tsx
+++ apps/web/src/components/MyPage/Usage/ApproveUsage/ApproveUsage.tsx
@@ -1,4 +1,4 @@
-import { HStack, Center, Box, VStack, Button, Text } from '@chakra-ui/react';
+import { HStack, Center, Box, VStack, Button, Text, Wrap } from '@chakra-ui/react';
 import { useContext, useState, useEffect } from 'react';
 import { FamilyService } from '@/__generated__/v1/odachin/faimily_pb';
 import { UsageService, GetUsageApplicationResponse } from '@/__generated__/v1/odachin/usage_pb';
@@ -50,50 +50,48 @@ export function ApproveUsage({}: ApproveUsageProps) {
     await usageClient.approveUsage({ usageId: usageId });
   };
   return (
-    <>
-      <HStack gap={8} mb={24} w='100%'>
-        {usageApplication?.usageApplications.map((usage) => (
-          <Center key={usage.usageId} py={6}>
-            <Box bg='white' boxShadow='2xl' overflow='hidden' rounded='md' w='330px'>
-              <VStack align='center' direction='row' justify='center'>
-                <HStack>
-                  <Text fontSize='3xl'>{usage.amount}</Text>
-                  <Text>pt</Text>
-                </HStack>
-                <Text fontSize='4xl' fontWeight={800}>
-                  {usage.title}
-                </Text>
-              </VStack>
+    <Wrap gap={8} m={10} w='100%'>
+      {usageApplication?.usageApplications.map((usage) => (
+        <Center key={usage.usageId} py={6}>
+          <Box bg='white' boxShadow='2xl' overflow='hidden' rounded='md' w='340px'>
+            <VStack align='center' direction='row' justify='center'>
+              <HStack>
+                <Text fontSize='3xl'>{usage.amount}</Text>
+                <Text>pt</Text>
+              </HStack>
+              <Text fontSize='4xl' fontWeight={800}>
+                {usage.title}
+              </Text>
+            </VStack>
 
-              <Box bg='gray.100' px={6} py={10}>
-                <Text>{usage.description}</Text>
+            <Box bg='gray.100' px={6} py={10}>
+              <Text>{usage.description}</Text>
 
-                <Button
-                  _focus={{
-                    bg: 'orange.500',
-                  }}
-                  _hover={{
-                    bg: 'orange.500',
-                  }}
-                  bg='orange.400'
-                  boxShadow='0 5px 20px 0px'
-                  color='white'
-                  fontWeight={600}
-                  mt={10}
-                  onClick={() => {
-                    apploveUsageApplication(usage.usageId);
-                    window.location.reload();
-                  }}
-                  rounded='xl'
-                  w='full'
-                >
-                  承認する
-                </Button>
-              </Box>
+              <Button
+                _focus={{
+                  bg: 'orange.500',
+                }}
+                _hover={{
+                  bg: 'orange.500',
+                }}
+                bg='orange.400'
+                boxShadow='0 5px 20px 0px'
+                color='white'
+                fontWeight={600}
+                mt={10}
+                onClick={() => {
+                  apploveUsageApplication(usage.usageId);
+                  window.location.reload();
+                }}
+                rounded='xl'
+                w='full'
+              >
+                承認する
+              </Button>
             </Box>
-          </Center>
-        ))}
-      </HStack>
-    </>
+          </Box>
+        </Center>
+      ))}
+    </Wrap>
   );
 }
